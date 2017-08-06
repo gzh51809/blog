7b773a91ab95ca9c5b647e938aea09434fc0ad0f
toxichl <472590061@qq.com>
2017-08-06 23:07:56 +0800

    Rename all CN to EN

--- docs/config.js
+++ docs/config.js
@@ -1,10 +1,3 @@
-window.$docsify = {
-    loadSidebar: true,
-    loadNavbar: true,
-    subMaxLevel: 3,
-    repo: 'https://github.com/ulivz',
-    name: 'ulivz',
-    coverpage: true
-}
+
 
 // docsify serve ./docs
